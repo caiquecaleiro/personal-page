38922c651d5c5135b3fee445e931bce869d737a6
Caique Caleiro <caiquecaleiro@gmail.com>
2017-03-01 18:59:30 -0300

    Add cover image

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,16 @@ const config = {
           fallback: 'style-loader',
           use: 'css-loader'
         })
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 40000 }
+          },
+          'image-webpack-loader'
+        ]
       }
     ]
   },
