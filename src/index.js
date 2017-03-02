955ede8848d4eeb86fc8dda29d737c6f3ece5733
Caique Caleiro <caiquecaleiro@gmail.com>
2017-03-02 10:07:00 -0300

    Add flexbox navbar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import '../style/style.css';
+
+const toggleNav = document.querySelector('.toggleNav');
+const ul = document.querySelector('.flex-nav ul');
+
+toggleNav.addEventListener('click', (e) => {
+  if (ul.classList.contains('open')) {
+    ul.classList.remove('open');
+  } else {
+    ul.classList.add('open');
+  }
+});
