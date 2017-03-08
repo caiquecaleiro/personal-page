859fdb9aab46631df6d8e7f2235bc6831d2ac4ff
Caique Caleiro <caiquecaleiro@gmail.com>
2017-03-08 15:04:38 -0300

    Add language skills

---
+++ src/data.js
@@ -0,0 +1,24 @@
+export const DEV_SKILLS = [
+  { description: 'JavaScript', rating: 4 },
+  { description: 'Angular', rating: 4 },
+  { description: 'HTML5', rating: 4 },
+  { description: 'CSS3', rating: 4 },
+  { description: 'React', rating: 3 },
+  { description: 'Java', rating: 3 },
+  { description: 'NodeJS', rating: 2 },
+  { description: 'MongoDB', rating: 2 },
+  { description: 'PL/SQL', rating: 2 },
+  { description: 'PHP', rating: 1 }
+];
+
+export const LANGUAGES = [
+  { description: 'Portuguese', rating: 5 },
+  { description: 'English', rating: 4 },
+  { description: 'German', rating: 1 }
+];
+
+export const EXPERIENCES = [
+  { company: 'Senior Sistemas', period: 'Jun 2016 - Feb 2017', position: 'Front End Developer', location: 'Blumenau, Brazil', description: 'Senior Sistemas is one of the largest software companies in Brazil. There I was helping on the development of new features for the ERP, using AngularJS and Metronic design. I also have motivated my teammates to study and to build prototypes using new technologies, such as ES6, React, NodeJS and MongoDB.' },
+  { company: 'Philips', period: 'Jun 2015 - Jun 2016', position: 'Development Lead', location: 'Blumenau, Brazil', description: 'I was responsible to lead and help the teams on the most important project at Philips Brazil, the Tasy HTML5 Health System. I have led and helped over 80 developers in Brazil and worked in India for two months, leading a team with 20 developers.', },
+  { company: 'Philips', period: 'Oct 2013 - Jun 2016', position: 'Front End Developer', location: 'Blumenau, Brazil - Bengaluru, India', description: 'Philips has developed the Tasy Health System, which is being used by over 1000 hospitals and health clinics in Latin America. As a Software Engineer, I have developed new features to help on the software internationalization for the Latin America and I have worked on many projects for Desktop, Web and Mobile platforms.' }
+];
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import '../style/style.css';
+import { DEV_SKILLS, LANGUAGES, EXPERIENCES } from './data';
 
 const toggleNav = document.querySelector('.toggleNav');
 const ul = document.querySelector('.flex-nav ul');
@@ -12,27 +13,9 @@ const experience = document.querySelector('#experience');
 const volunteer = document.querySelector('#volunteer');
 const contact = document.querySelector('#contact');
 
-const experiences = [
-  { company: 'Senior Sistemas', period: 'Jun 2016 - Feb 2017', position: 'Front End Developer', location: 'Blumenau, Brazil', description: 'Senior Sistemas is one of the largest software companies in Brazil. There I was helping on the development of new features for the ERP, using AngularJS and Metronic design. I also have motivated my teammates to study and to build prototypes using new technologies, such as ES6, React, NodeJS and MongoDB.' },
-  { company: 'Philips', period: 'Jun 2015 - Jun 2016', position: 'Development Lead', location: 'Blumenau, Brazil', description: 'I was responsible to lead and help the teams on the most important project at Philips Brazil, the Tasy HTML5 Health System. I have led and helped over 80 developers in Brazil and worked in India for two months, leading a team with 20 developers.', },
-  { company: 'Philips', period: 'Oct 2013 - Jun 2016', position: 'Front End Developer', location: 'Blumenau, Brazil - Bengaluru, India', description: 'Philips has developed the Tasy Health System, which is being used by over 1000 hospitals and health clinics in Latin America. As a Software Engineer, I have developed new features to help on the software internationalization for the Latin America and I have worked on many projects for Desktop, Web and Mobile platforms.' }
-];
-
-const skillsList = [
-  { description: 'JavaScript', rating: 4 },
-  { description: 'Angular', rating: 4 },
-  { description: 'HTML5', rating: 4 },
-  { description: 'CSS3', rating: 4 },
-  { description: 'React', rating: 3 },
-  { description: 'Java', rating: 3 },
-  { description: 'NodeJS', rating: 2 },
-  { description: 'MongoDB', rating: 2 },
-  { description: 'PL/SQL', rating: 2 },
-  { description: 'PHP', rating: 1 },
-] 
-
 addExperiences();
-addSkills();
+addDevelopmentSkills();
+addLanguageSkills();
 
 toggleNav.addEventListener('click', (e) => {
   if (ul.classList.contains('open')) {
@@ -69,7 +52,7 @@ function onScroll(event) {
 }
 
 function addExperiences() {
-  const html = experiences.map(exp => {
+  const html = EXPERIENCES.map(exp => {
     return `
       <div class="experience">
         <div class="company-info">
@@ -92,10 +75,10 @@ function addExperiences() {
   experience.innerHTML += html;
 }
 
-function addSkills() {
-  const html = skillsList.map(skill => {
+function addDevelopmentSkills() {
+  const html = DEV_SKILLS.map(skill => {
     return `
-      <li>
+      <li class="skill-item">
         <span class="skill-description">${skill.description}</span>
         <span class="skill-rating">${renderRating(skill.rating)}</span>
       </li>
@@ -104,6 +87,20 @@ function addSkills() {
   .join('');
 
   document.querySelector('.skill-list').innerHTML = html;
+}
+
+function addLanguageSkills() {
+  const html = LANGUAGES.map(lang => {
+    return `
+      <li class="skill-item">
+        <span class="skill-description">${lang.description}</span>
+        <span class="skill-rating">${renderRating(lang.rating)}</span>
+      </li>
+    `;
+  })
+  .join('');
+
+  document.querySelector('.language-list').innerHTML = html;
 }
 
 function renderRating(rating) {
