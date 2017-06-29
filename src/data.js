de9d2d9ae1a3136dfbaeebb160e9491b16ec60bf
Caique Caleiro <caiquecaleiro@gmail.com>
2017-06-29 21:42:25 +1200

    Update website content

--- src/data.js
+++ src/data.js
@@ -1,25 +1,26 @@
 export const DEV_SKILLS = [
   { description: 'JavaScript', rating: 4 },
+  { description: 'React', rating: 4 },
   { description: 'Angular', rating: 4 },
   { description: 'HTML5', rating: 4 },
   { description: 'CSS3', rating: 4 },
   { description: 'SQL', rating: 4 },
-  { description: 'React', rating: 3 },
+  { description: 'Webpack', rating: 3 },
+  { description: 'NodeJS', rating: 3 },
   { description: 'Java', rating: 3 },
-  { description: 'NodeJS', rating: 2 },
   { description: 'MongoDB', rating: 2 },
   { description: 'GraphQL', rating: 2 },
-  { description: 'PL/SQL', rating: 2 },
-  { description: 'PHP', rating: 1 }
+  { description: 'PL/SQL', rating: 2 }
 ];
 
 export const LANGUAGES = [
+  { description: 'English', rating: 5 },
   { description: 'Portuguese', rating: 5 },
-  { description: 'English', rating: 4 },
   { description: 'German', rating: 1 }
 ];
 
 export const EXPERIENCES = [
+  { company: 'Pixel Fusion', period: 'May 2017 - Now', position: 'Front End Developer', location: 'Auckland, New Zealand', description: 'I am working together with designers and developers to build great digital products. As a developer, I am helping the team with the development of new products using React, NodeJS, Atomic Design, SASS and TDD.' },
   { company: 'Senior Sistemas', period: 'Jun 2016 - Feb 2017', position: 'Front End Developer', location: 'Blumenau, Brazil', description: 'Senior Sistemas is one of the largest software companies in Brazil. There I was helping on the development of new features for the ERP, using AngularJS and Metronic design. I also have motivated my teammates to study and to build prototypes using new technologies, such as ES6, React, NodeJS and MongoDB.' },
   { company: 'Philips', period: 'Jun 2015 - Jun 2016', position: 'Development Lead', location: 'Blumenau, Brazil - Bengaluru, India', description: 'I was responsible to lead and help the teams on the most important project at Philips Brazil, the Tasy HTML5 Health System. I have led and helped over 80 developers in Brazil and worked in India for two months, leading a team with 20 developers.', },
   { company: 'Philips', period: 'Oct 2013 - Jun 2016', position: 'Software Engineer', location: 'Blumenau, Brazil', description: 'Philips has developed the Tasy Health System, which is being used by over 1000 hospitals and health clinics in Latin America. As a Software Engineer, I have developed new features to help on the software internationalization for the Latin America and I have worked on many projects for Desktop, Web and Mobile platforms.' }
