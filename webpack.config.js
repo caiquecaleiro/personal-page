c1ca168eb42fdfc3bbbeb04183c2143a1d38a6d1
Caique Caleiro <caiquecaleiro@gmail.com>
2018-01-05 20:12:00 +1300

    Add support to sass, stylelint and editorconfig

--- webpack.config.js
+++ webpack.config.js
@@ -4,46 +4,53 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const config = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[chunkhash].js'
-  },
-  devServer: {
-    port: 4000
-  },
-  module: {
-    rules: [
-      {
-        use: 'babel-loader',
-        test: /\.js$/
-      },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'postcss-loader']
-        })
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/,
-        use: ['url-loader', {
-          loader: 'image-webpack-loader',
-          options: {}
-        }]
-      }
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.[chunkhash].js'
+    },
+    devServer: {
+        port: 4000
+    },
+    module: {
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.js$/
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: ExtractTextPlugin.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            query: { importLoaders: 1, sourceMap: true }
+                        },
+                        {
+                            loader: 'postcss-loader'
+                        }
+                    ]
+                })
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                use: ['url-loader', {
+                    loader: 'image-webpack-loader',
+                    options: {}
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('styles.[contenthash].css'),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new CopyWebpackPlugin([
+            { from: 'CNAME' },
+            { from: 'img/favicon.ico' }
+        ])
     ]
-  },
-  plugins: [
-    new ExtractTextPlugin('styles.[contenthash].css'),
-    new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    }),
-    new CopyWebpackPlugin([
-      { from: 'CNAME' },
-      { from: 'img/favicon.ico' }
-    ])
-  ]
 };
 
 module.exports = config;
